c59942234bf6a6bb475a82be0c5bbc63cb5f38b9
jean <jpzuniga@est.itsgg.edu.ec>
2020-09-02 15:16:55 -0500

    A correr

--- cliente/urls.py
+++ cliente/urls.py
@@ -4,4 +4,6 @@ from cliente import views
 urlpatterns = [
     path('cliente/', views.cliente, name="cliente"),
     path('crearcliente/', views.crearcliente, name="crearcliente"),
+    path('modificarcliente/<int:pk>', views.modificarcliente, name="modificarcliente"),
+    path('eliminarcliente/<int:pk>', views.eliminarcliente, name="eliminarcliente"),
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 
 # Create your views here.
 from .forms import clienteForm, Cliente
@@ -8,7 +8,8 @@ def cliente(request, plantilla="cliente.html"):
         'cliente':clientes
     }
     return render(request, plantilla, data)
-#pagina de crear o insertar INSERT
+
+##   pagina de crear o insertar INSERT
 def crearcliente(request, plantilla="crearcliente.html"):
 
     if request.method == "POST":
@@ -18,5 +19,35 @@ def crearcliente(request, plantilla="crearcliente.html"):
             return redirect('clientes')
     else:
         form = clienteForm
+
+    return render(request, plantilla, {'form': form})
+
+## pagina de modificar o update
+def modificarcliente(request, pk, plantilla="modificarcliente.html"):
+
+    if request.method == "POST":
+        cliente = get_object_or_404(Cliente, pk=pk)
+        form = clienteForm(request.POST or None, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('cliente')
+    else:
+        cliente = get_object_or_404(Cliente, pk=pk)
+        form = clienteForm(request.POST or None, instance=cliente)
+
+    return render(request, plantilla, {'form': form})
+
+## pagina de eliminar o delete
+def eliminarcliente(request, pk, plantilla="eliminarcliente.html"):
+
+    if request.method == "POST":
+        form = clienteForm((request.POST or None))
+        cliente = get_object_or_404(Cliente, pk=pk)
+        if form.is_valid():
+            cliente.delete()
+            return redirect('cliente')
+    else:
+        cliente = get_object_or_404(Cliente, pk=pk)
+        form = clienteForm(request.POST or None, instance=cliente)
 
     return render(request, plantilla, {'form': form})
